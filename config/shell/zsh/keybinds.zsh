6a2da01e353a337292a4b5978a36108e4b5e0e2d
Owen Pierce <sresarehumantoo@users.noreply.github.com>
2026-03-06 11:21:48 -0500

    Fix bugs

--- config/shell/zsh/keybinds.zsh
+++ config/shell/zsh/keybinds.zsh
@@ -13,17 +13,17 @@ bindkey -M vicmd 'G' end-of-buffer-or-history
 bindkey -M vicmd 'gg' beginning-of-buffer-or-history
 bindkey -M vicmd '/' history-incremental-search-backward
 
-# Vi-mode cursor shape: block in normal, beam in insert
+# Vi-mode cursor shape: steady block in normal, blinking block in insert
 function zle-keymap-select {
   if [[ $KEYMAP == vicmd ]] || [[ $1 == 'block' ]]; then
-    echo -ne '\e[2 q'  # block cursor
+    echo -ne '\e[2 q'  # steady block
   elif [[ $KEYMAP == main ]] || [[ $KEYMAP == viins ]] || [[ $1 == 'beam' ]]; then
-    echo -ne '\e[6 q'  # beam cursor
+    echo -ne '\e[1 q'  # blinking block
   fi
 }
 zle -N zle-keymap-select
 
-# Start with beam cursor
-function _set_beam_cursor { echo -ne '\e[6 q' }
-_set_beam_cursor
-precmd_functions+=(_set_beam_cursor)
+# Start with blinking block cursor
+function _set_block_cursor { echo -ne '\e[1 q' }
+_set_block_cursor
+precmd_functions+=(_set_block_cursor)
